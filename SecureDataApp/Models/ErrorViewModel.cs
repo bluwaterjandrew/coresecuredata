822649a4ab03d717e44af3550b7a44282518cd20
Jake <jakedewerth@gmail.com>
2018-08-23 18:44:09 -0400

    Add project files.

---
+++ SecureDataApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecureDataApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
